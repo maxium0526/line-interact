920068e861c897af4b40043d357c409c9ff2fd8c
Max Chong <maxium0526@github.com>
2020-06-27 03:52:03 +0800

    implement the isIntersected method sucessfully

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ class Point{
 		this.y = y;
 	}
 
-	corotate(x = 0, y = 0, angle = 0){
+	corotate(x, y, angle){
 		if(x == this.x && y == this.y) return new Point(x, y);
 
 		let length2 = Math.pow(x-this.x, 2) + Math.pow(y-this.y, 2);//distance between itself and origin
@@ -21,9 +21,17 @@ class Point{
 		return new Point(this.x+x, this.y+y);
 	}
 
-	getAngle(x = 0, y = 0){ //angle between the line of 2 points and x-axis
-		let angle = Math.atan((this.y-y) / (this.x-x));
-		return (angle + Math.PI) % Math.PI; //always return positive
+	getAngle(x, y){ //angle between the line of 2 points and x-axis
+		let slape = Math.atan((this.y-y) / (this.x-x));//this is just slape
+
+		let angle = null;
+		let xOffset = this.x - x;
+		let yOffset = this.y - y;
+		if(xOffset>0 && yOffset>0) angle = slape;//第一象限
+		if(xOffset<0 && yOffset>0) angle = Math.PI + slape;//第二象限
+		if(xOffset<0 && yOffset<0) angle = Math.PI + slape;//第三象限
+		if(xOffset>0 && yOffset<0) angle = 2 * Math.PI + slape;//第四象限
+		return angle;
 	}
 
 	equals(p){
@@ -52,6 +60,12 @@ class Line{
 		return new Line(this.p1.translate(x, y), this.p2.translate(x, y));
 	}
 
+	project(axis){
+		if(axis = 'x') return new Line(new Point(this.p1.x, 0), new Point(this.p2.x, 0));
+		if(axis = 'y') return new Line(new Point(0, this.p1.y), new Point(0, this.p2.y));
+		return null;
+	}
+
 	getAngle(){//the angle between itself and x-axis. In other words, atan(slape).
 		return this.p1.getAngle(this.p2.x, this.p2.y);//use getAngle() of Point class, another point as origin point.
 	}
@@ -68,9 +82,55 @@ class Line{
 		return this;
 	}
 
+	static isOverlappedOnAxis(a, b, axis){
+		if(!['x', 'y'].includes(axis)) return null;
+		let pa = a.project(axis);
+		let pb = b.project(axis);
 
+		//find the max and min of two line in order to compare
+		let amax, amin, bmax, bmin;
+		if(axis == 'x'){
+			amax = Math.max(a.p1.x, a.p2.x);
+			amin = Math.min(a.p1.x, a.p2.x);
+			bmax = Math.max(b.p1.x, b.p2.x);
+			bmin = Math.min(b.p1.x, b.p2.x);
+		} else {
+			amax = Math.max(a.p1.y, a.p2.y);
+			amin = Math.min(a.p1.y, a.p2.y);
+			bmax = Math.max(b.p1.y, b.p2.y);
+			bmin = Math.min(b.p1.y, b.p2.y);
+		}
 
-	
+		if(amax < bmin || bmax < amin){
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	static isIntersected(a, b){
+		if(Line.__isOverLappedOnRotatedAxis__(a, b) && Line.__isOverLappedOnRotatedAxis__(b, a)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	static __isOverLappedOnRotatedAxis__(line, referenceLine){
+		//rotation process
+		let rotatePoint = new Point(0, 0);
+		let rotateAngle = referenceLine.getAngle();
+		let rotatedLine = line.corotate(rotatePoint.x, rotatePoint.y, -rotateAngle);
+		let rotatedReferenceLine = referenceLine.corotate(rotatePoint.x, rotatePoint.y, -rotateAngle);
+
+		//translation process
+		// let tnsPoint = rotatedReferenceLine.p1;
+		// let tnsedLine = rotatedLine.translate(-tnsPoint.x, -tnsPoint.y);
+		// let tnsedReferenceLine = rotatedReferenceLine.translate(-tnsPoint.x, -tnsPoint.y);
+		//有沒有都一樣啦所以註解了
+
+		return Line.isOverlappedOnAxis(rotatedLine, rotatedReferenceLine, 'y');
+	}
 }
 
 var canvas = document.getElementById('canvas');
@@ -79,32 +139,19 @@ ctx.translate(200, 200);
 
 prepareCoordinate();
 
-var lines = [
-	new Line(new Point(50, 50), new Point(70,90)),
-	new Line(new Point(-30, 10), new Point(-180, 70))
-];
+var line1 = randomLine().draw(ctx);
+var line2 = randomLine().draw(ctx);
 
-for(let line of lines){
-	line.draw(ctx);
-}
-
-var line1angle = lines[0].getAngle();
-var corotatedLines = lines.map(line=>line.corotate(0, 0, Math.PI / 2 - line1angle));
-
-var line1p1 = corotatedLines[0].p1;
-var translatedLines = corotatedLines.map(line=>line.translate(-line1p1.x, -line1p1.y))
-
-ctx.fillStyle = "#0000FF";
-ctx.strokeStyle = "#0000FF";
-
-for(let line of translatedLines){
-	line.draw(ctx);
-}
+console.log(Line.isIntersected(line1, line2));
 
 console.log("Black: Before rotate.");
-console.log("Blue: After rotate.")
+console.log("Blue: After rotate.");
 
+// var line = randomLine().draw(ctx);
 
+// ctx.fillStyle = "#0000FF";
+// ctx.strokeStyle = "#0000FF";
+// line.corotate(0,0,Math.PI / 4).draw(ctx);
 
 
 
@@ -118,4 +165,11 @@ function prepareCoordinate(){
 	ctx.moveTo(0,-200);
 	ctx.lineTo(0,200);
 	ctx.stroke();
+}
+
+function randomLine(){
+	return new Line(
+		new Point(Math.random()*200-100, Math.random()*200-100),
+		new Point(Math.random()*200-100, Math.random()*200-100),
+		);
 }
