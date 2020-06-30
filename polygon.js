93c2f8568fc16b08bcd0687a90daba2c55be16c0
Max Chong <maxium0526@github.com>
2020-07-01 00:21:23 +0800

    add draw for Polygon class

--- polygon.js
+++ polygon.js
@@ -18,4 +18,14 @@ class Polygon{
 	getSlape(lineNum){
 		return new Line(this.points[lineNum], this.points[lineNum+1]).getSlape();
 	}
+
+	draw(ctx){
+		for(let point of this.points){
+			point.draw(ctx);
+		}
+		for(let i=0; i<this.points.length; i++){
+			new Line(this.points[i], this.points[(i+1)%(this.points.length)]).draw(ctx);
+		}
+		return this;
+	}
 }
