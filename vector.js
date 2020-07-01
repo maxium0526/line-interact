0d6a48549917c8a1c0ced98ac8fe454987a381bb
Max Chong <maxium0526@github.com>
2020-07-02 03:17:11 +0800

    add Vector draw()

--- vector.js
+++ vector.js
@@ -15,5 +15,15 @@ class Vector{
 	dot(v){
 		return this.x * v.x + this.y * v.y;
 	}
-	
+
+	draw(ctx, x=0, y=0){
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		ctx.lineTo(this.x, this.y);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.arc(x, y, 5, 0, 2 * Math.PI);
+		ctx.fill();
+		return this;
+	}
 }
