5a7c8c854cae5d80014711bd3b996638d5613743
Max Chong <maxium0526@github.com>
2020-07-02 19:13:54 +0800

    complete PolygonItem static touch() v1, still many bugs

--- polygon.js
+++ polygon.js
@@ -128,6 +128,15 @@ class PolygonItem{
 	getCenter(){
 		return this.poly.getSimpleCentroid();
 	}
+	getLinearVelocity(x, y){
+		let r = new Line(this.getCenter(), new Point(x, y));
+		let linearVelo = this.angVelo * r.getLength();
+		let angle = r.getAngle() + Math.PI / 2;
+		return Vector.get(linearVelo, angle);
+	}
+	getSumVelocity(x, y){
+		return this.getLinearVelocity(x, y).add(this.velo);
+	}
 	nxt(fps){
 		this.poly = this.poly
 			.translate(this.velo.x / fps, this.velo.y / fps)
@@ -143,11 +152,62 @@ class PolygonItem{
 		let intersectedPoints = Polygon.calcIntersectionPoint(a.poly, b.poly);
 		let intersectedCentroid = new Polygon(intersectedPoints).getSimpleCentroid();
 
-		let ax = (a.velo.x * (a.mass - b.mass) + 2 * b.mass * b.velo.x) / (a.mass + b.mass);		
-		let ay = (a.velo.y * (a.mass - b.mass) + 2 * b.mass * b.velo.y) / (a.mass + b.mass);
-		let bx = (b.velo.x * (b.mass - a.mass) + 2 * a.mass * a.velo.x) / (a.mass + b.mass);
-		let by = (b.velo.y * (b.mass - a.mass) + 2 * a.mass * a.velo.y) / (a.mass + b.mass);
-		a.velo = new Vector(ax, ay);
-		b.velo = new Vector(bx, by);
+		// let ax = (a.velo.x * (a.mass - b.mass) + 2 * b.mass * b.velo.x) / (a.mass + b.mass);		
+		// let ay = (a.velo.y * (a.mass - b.mass) + 2 * b.mass * b.velo.y) / (a.mass + b.mass);
+		// let bx = (b.velo.x * (b.mass - a.mass) + 2 * a.mass * a.velo.x) / (a.mass + b.mass);
+		// let by = (b.velo.y * (b.mass - a.mass) + 2 * a.mass * a.velo.y) / (a.mass + b.mass);
+		// a.velo = new Vector(ax, ay);
+		// b.velo = new Vector(bx, by); 
+		
+		let x = intersectedCentroid.x;
+		let y = intersectedCentroid.y;
+
+		//碰撞點的混合速度
+		let av = a.getSumVelocity(x, y);
+		let bv = b.getSumVelocity(x, y);
+
+		//碰撞後各自得到的速度
+		let ax = (av.x * (a.mass - b.mass) + 2 * b.mass * bv.x) / (a.mass + b.mass);		
+		let ay = (av.y * (a.mass - b.mass) + 2 * b.mass * bv.y) / (a.mass + b.mass);
+		let bx = (bv.x * (b.mass - a.mass) + 2 * a.mass * av.x) / (a.mass + b.mass);		
+		let by = (bv.y * (b.mass - a.mass) + 2 * a.mass * av.y) / (a.mass + b.mass);
+
+		console.log(ax, ay, bx, by)
+
+		let getA = new Vector(ax, ay);
+		let getB = new Vector(bx, by);
+
+		console.log(getA, getB)
+
+		let ra = new Line(a.getCenter(), new Point(x, y));
+		let rb = new Line(a.getCenter(), new Point(x, y));
+
+		//轉到十字架和座標軸平行
+		getA = getA.rotate(ra.getAngle());
+		getB = getB.rotate(rb.getAngle());
+
+		console.log(getA, getB)
+
+		//開拆
+		let getAAngVelo = new Vector(0, getA.y).rotate(-ra.getAngle());
+		let getAVelo = new Vector(getA.x, 0).rotate(-ra.getAngle());
+
+		let getBAngVelo = new Vector(0, getB.y).rotate(-rb.getAngle());
+		let getBVelo = new Vector(getB.x, 0).rotate(-rb.getAngle());
+
+
+		a.velo = getAVelo;
+		a.angVelo = getAAngVelo.y;	
+
+		console.log(a, b)
+
+		b.velo = getBVelo;
+		b.angVelo = getBAngVelo.y;
+
+		//碰撞後立即分開, 防止多之碰撞
+		while(Polygon.isIntersected(a.poly, b.poly)){
+			a.poly = a.poly.translate((a.getCenter().x - intersectedCentroid.x) / 100, (a.getCenter().y - intersectedCentroid.y) / 100)
+			b.poly = b.poly.translate((b.getCenter().x - intersectedCentroid.x) / 100, (b.getCenter().y - intersectedCentroid.y) / 100)
+		}
 	}
 }
